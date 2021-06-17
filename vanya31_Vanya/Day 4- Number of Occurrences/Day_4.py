787071a3cc62c84f9aa86ef23b6e28133aff6eb7
vanya_31 <vanya.verma31@gmail.com>
2021-06-17 13:47:49 +0530

    Day4

---
+++ vanya31_Vanya/Day 4- Number of Occurrences/Day_4.py
@@ -0,0 +1,8 @@
+def array_occur():
+    arr=[]
+    n=int(input("Enter number of elements in array:"))
+    for i in range(n):
+        print("Enter array elements")
+        ele=int(input())
+
+    arr.append(ele)
