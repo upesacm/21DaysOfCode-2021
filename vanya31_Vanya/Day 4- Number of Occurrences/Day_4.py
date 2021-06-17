b010ebd332e3f8146323de6e3c76c2d1345314f9
vanya_31 <vanya.verma31@gmail.com>
2021-06-17 20:43:52 +0530

    Python Array Reccurrence

--- vanya31_Vanya/Day 4- Number of Occurrences/Day_4.py
+++ vanya31_Vanya/Day 4- Number of Occurrences/Day_4.py
@@ -1,8 +1,18 @@
+arr=[]
 def array_occur():
-    arr=[]
     n=int(input("Enter number of elements in array:"))
+    print("Enter array element:")
     for i in range(n):
-        print("Enter array elements")
-        ele=int(input())
+     ele=int(input())
+     arr.append(ele)
 
-    arr.append(ele)
+    search = int(input("Enter the element to search:"))
+    occur = arr.count(search)
+    print("Number of times the element occurred is: ",occur)
+
+
+array_occur()
+
+
+
+
