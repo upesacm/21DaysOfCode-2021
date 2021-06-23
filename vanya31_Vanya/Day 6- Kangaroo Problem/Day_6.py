7e1f6d71be16acf45b05a01d685d04bf576b6879
vanya_31 <vanya.verma31@gmail.com>
2021-06-23 22:35:57 +0530

    Contest Solution

--- vanya31_Vanya/Day 6- Kangaroo Problem/Day_6.py
+++ vanya31_Vanya/Day 6- Kangaroo Problem/Day_6.py
@@ -21,27 +21,16 @@ def kangaroo(x1, v1, x2, v2):
     # My Solution
     for x in range(1000):
      if ((x1+v1)==(x2+v2)):
-        return "YES"
+        print("YES")
      x1=x1+v1
      x2=x2+v2
-    return "NO"
+    print("NO")
 
 #Driver Code
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+first_multiple_input = input().rstrip().split()
+x1 = int(first_multiple_input[0])
+v1 = int(first_multiple_input[1])
+x2 = int(first_multiple_input[2])
+v2 = int(first_multiple_input[3])
 
-    first_multiple_input = input().rstrip().split()
-
-    x1 = int(first_multiple_input[0])
-
-    v1 = int(first_multiple_input[1])
-
-    x2 = int(first_multiple_input[2])
-
-    v2 = int(first_multiple_input[3])
-
-    result = kangaroo(x1, v1, x2, v2)
-
-    fptr.write(result + '\n')
-
-    fptr.close()
+kangaroo(x1, v1, x2, v2)
