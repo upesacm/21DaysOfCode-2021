24c38d2f74c13721d10daa1532947a9b8ea06819
vanya_31 <vanya.verma31@gmail.com>
2021-06-29 11:55:01 +0530

    Day 12 and 13

---
+++ vanya31_Vanya/Day 12- Cats and Mouse/Day_12.py
@@ -0,0 +1,37 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the catAndMouse function below.
+# My solution
+def catAndMouse(x, y, z):
+    if (abs(z - y) == abs(z - x)):
+        return "Mouse C"
+    elif (abs(z - y) > abs(z - x)):
+        return "Cat A"
+    elif (abs(z - y) < abs(z - x)):
+        return "Cat B"
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        xyz = input().split()
+
+        x = int(xyz[0])
+
+        y = int(xyz[1])
+
+        z = int(xyz[2])
+
+        result = catAndMouse(x, y, z)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
---
+++ vanya31_Vanya/Day 12- Cats and Mouse/README.md
@@ -0,0 +1,2 @@
+https://www.hackerrank.com/challenges/cats-and-a-mouse/problem
+used the concept of distance and abs() function learned previously.
